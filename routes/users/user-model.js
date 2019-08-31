8ac73d3ee6309056ae41913a8e7870185e5adeec
LadyKerr <kadiekerr@gmail.com>
2019-08-30 21:52:08 -0400

    user-model complete

---
+++ routes/users/user-model.js
@@ -0,0 +1,51 @@
+const db = require("../../data/db-config");
+
+module.exports = {
+  fetchUsers,
+  fetchById,
+  add,
+  remove,
+  update
+};
+
+//fetch all users
+function fetchUsers() {
+  return db("users");
+}
+
+//fetch users by id
+function fetchById(id) {
+  return db("users")
+    .where({ id })
+    .first()
+    .then(user => {
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    });
+}
+
+//add user
+function add(newUser) {
+  return db("users")
+    .insert(newUser)
+    .then(id => {
+      return fetchById(id[0]);
+    });
+}
+
+//delete user
+function remove(id) {
+  return db("users")
+    .where({ id })
+    .del();
+}
+
+//update user
+function update(changes, id) {
+  return db("users")
+    .where({ id })
+    .update(changes);
+}
