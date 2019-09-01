122ef04fe389edfc0b049ae0a4cf6789227ea8a7
Jose Montero <josemariodev@MacBookPro.attlocal.net>
2019-09-01 13:54:50 -0500

    PUT working for users, scaffold test file

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,1 +1,17 @@
-//setup tests for all endpoints here
+
+const request = require('supertest');
+
+const db = require('../database/dbConfig.js');
+const server = require('../api/server');
+
+describe('server', () => {
+  beforeEach(async () => {
+    await db('users').truncate();
+  });
+
+  it("tests are running with DB_ENV set to 'testing'", () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  });;
+
+
+});
