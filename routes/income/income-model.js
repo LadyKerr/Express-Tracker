7a3fc950d1ca4a6e0c4517f1a3117e408b087863
LadyKerr <kadiekerr@gmail.com>
2019-09-01 10:51:31 -0400

    income model

---
+++ routes/income/income-model.js
@@ -0,0 +1,50 @@
+const db = require("../../data/db-config");
+
+module.exports = {
+  fetchIncome,
+  fetchBy,
+  fetchById,
+  add,
+  remove,
+  update
+};
+
+//fetch all income
+function fetchIncome() {
+  return db("income").select("*");
+}
+
+//fetch income by id
+function fetchById(id) {
+  return db("income")
+    .where({ id })
+    .first();
+}
+
+//fetchBy specified filter
+function fetchBy(filter) {
+  return db("income").where(filter);
+}
+
+//add income
+function add(newIncome) {
+  return db("income")
+    .insert(newIncome)
+    .then(id => {
+      return fetchById(id[0]);
+    });
+}
+
+//delete income
+function remove(id) {
+  return db("income")
+    .where({ id })
+    .del();
+}
+
+//update income
+function update(changes, id) {
+  return db("income")
+    .where({ id })
+    .update(changes);
+}
